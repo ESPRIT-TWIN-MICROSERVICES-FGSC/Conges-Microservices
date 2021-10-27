5f26585d6687be0ad39c88765dacefe27a88add1
Ghada Khedri <61046136+ghadakhedri1@users.noreply.github.com>
2021-10-27 15:43:48 +0100

    Update Conges.java

--- src/main/java/esprit/fgsc/congesmicroservice/entities/Conges.java
+++ src/main/java/esprit/fgsc/congesmicroservice/entities/Conges.java
@@ -45,6 +45,12 @@ public String getId() {
 public void setId(String id) {
 	this.id = id;
 }
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
 public String getLastName() {
 	return lastName;
 }
